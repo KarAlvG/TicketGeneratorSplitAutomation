feb16457bf79426ab2c866458dbb40409e01d8d8
KarAlvG <karalvarezg24@gmail.com>
2025-10-03 20:21:31 +0200

    Commit: TicketGeneratorSplitAutomation second version Create Folder with random Ticket number in txt File

--- Program.cs
+++ Program.cs
@@ -11,8 +11,36 @@ try
         Directory.CreateDirectory(folderPath);
         Console.WriteLine($"Folder created at: {folderPath}");
     }
-    File.WriteAllText(filePath, "test");
-    Console.WriteLine($"File generated succesfully at: {filePath}");
+
+    Random random = new Random();
+
+    int length = random.Next(6, 9);
+
+    char[] ticket = new char[length];
+
+    ticket[0] = (char)('0' + random.Next(1, 4));
+
+    if (random.Next(2) == 0)
+    {
+        ticket[1] = (char)('A' + random.Next(6));
+    }
+    else
+    {
+        ticket[1] = (char)('0' + random.Next(10));
+    }
+
+    for (int i = 2; i < length; i++)
+    {
+        ticket[i] = (char)('0' + random.Next(10));
+    }
+
+    string ticketString = new string(ticket);
+
+
+    File.WriteAllText(filePath, ticketString);
+    Console.WriteLine($"Ticket generated succesfully at: {ticketString}");
+    Console.WriteLine($"Saved at {filePath}");
+    
 }
 catch (Exception ex)
 {
