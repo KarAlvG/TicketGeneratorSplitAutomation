8c92f3b054cc841cade80f55a21a44fc7669e019
KarAlvG <karalvarezg24@gmail.com>
2025-10-03 14:30:53 +0200

    Initial commit: TicketGeneratorSplitAutomation first version Create Folder with txt File

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+string folderPath = @"C:\FileGenerator";
+string filePath = Path.Combine(folderPath, "output.txt");
+
+try
+{
+    if (!Directory.Exists(folderPath))
+    {
+        Directory.CreateDirectory(folderPath);
+        Console.WriteLine($"Folder created at: {folderPath}");
+    }
+    File.WriteAllText(filePath, "test");
+    Console.WriteLine($"File generated succesfully at: {filePath}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error ocurred");
+    Console.WriteLine(ex.Message);
+}
